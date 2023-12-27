821a6520355e56f32199a89f2b0f06482f37d238
Pasan Narangoda <narangodapasan@gmail.com>
2023-12-27 21:10:07 +0530

    added model, view, controller

---
+++ setup.py
@@ -0,0 +1,10 @@
+# from setuptools import setup
+#
+# setup(
+#     name='NetWatcher',
+#     packages=['NetWatcher'],
+#     include_package_data=True,
+#     install_requires=[
+#         'flask',
+#     ],
+# )
