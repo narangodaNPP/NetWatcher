49fe697ef9d9a6ea50e99078e410ce54f4102910
Pasan Narangoda <narangodapasan@gmail.com>
2023-12-27 23:45:44 +0530

    added dummy devices list and check output

--- NetWatcher/__init__.py
+++ NetWatcher/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import yaml
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def import_devices():
+    with open("NetWatcher/data/devices.yaml") as device_files:
+        devices = yaml.safe_load(device_files.read())
+    return devices
+
+
+@app.route("/devices/")
+def devices():
+
+    devices = import_devices()
+    return {"devices": devices}
